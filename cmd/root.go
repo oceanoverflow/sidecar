0de16c87a7d56adcd03e29630ec9e08ff05734e0
yiyangyi <oceanoverflow@gmail.com>
2018-05-18 15:14:31 +0800

    Change default config file path to use in container

--- cmd/root.go
+++ cmd/root.go
@@ -12,20 +12,20 @@ var etcd string
 var cfg string
 
 func init() {
-	cobra.OnInitialize(initConfig)
 	rootCmd.AddCommand(consumerCmd)
 	rootCmd.AddCommand(providerCmd)
 	rootCmd.PersistentFlags().StringVarP(&cfg, "cfg", "c", "", "config file")
 	rootCmd.PersistentFlags().StringVarP(&etcd, "etcd", "e", "", "etcd registry")
 	viper.BindPFlag("cfg", rootCmd.PersistentFlags().Lookup("cfg"))
 	viper.BindPFlag("etcd", rootCmd.PersistentFlags().Lookup("etcd"))
+	cobra.OnInitialize(initConfig)
 }
 
 func initConfig() {
 	if cfg != "" {
 		viper.SetConfigFile(cfg)
 	} else {
-		viper.SetConfigFile("/Users/oceanoverflow/Desktop/config.toml")
+		viper.SetConfigFile("/root/dists/config.toml")
 	}
 
 	if err := viper.ReadInConfig(); err != nil {
